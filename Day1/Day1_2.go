6976714e40b39949053ddcba070bdccc7b09a5ae
laiot <carmelo.sarta.main@gmail.com>
2019-12-05 10:50:52 +0100

    Added day 1, part 2.

--- Day1/Day1_2.go
+++ Day1/Day1_2.go
@@ -18,13 +18,24 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	var x float64
+	var x, z float64
 	x = 0
+	z = 0
 
 	for scanner.Scan() {
 		y, _ := strconv.ParseFloat(scanner.Text(), 64)
-		x += math.Floor(y/float64(3)) - 2
+		x = math.Floor(y/float64(3)) - 2
+		z += x
+
+		for {
+			x = math.Floor(x/float64(3)) - 2
+			if x <= 0 {
+				break
+			} else {
+				z += x
+			}
+		}
 	}
 
-	fmt.Println("P1: " + strconv.FormatFloat(x, 'E', -1, 64))
+	fmt.Println("P1: " + strconv.FormatFloat(z, 'E', -1, 64))
 }
