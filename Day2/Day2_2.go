2c58c5e8b910a9a7acc66ac01e18dbe4e147064c
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2019-12-06 14:35:45 +0100

    Update Day2_2.go
    Corrected out of bound error. 
    
    Go is weird guys.

--- Day2/Day2_2.go
+++ Day2/Day2_2.go
@@ -1,49 +1,54 @@
 package main
-
+ 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "strings"
 )
-
+ 
+ 
 func main() {
-	file, err := os.Open("Day2/in2.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	s := strings.Split(scanner.Text(), ",")
-	var nums [276]int
-	for a, b := range s {
-		nums[a], _ = strconv.Atoi(b)
-	}
-out:
-	for noun := 0; noun < 100; noun++ {
-		for verb := 0; verb < 100; verb++ {
-			i := 0
-			nums[1] = noun
-			nums[2] = verb
-			for {
-				if nums[i] == 1 {
-					nums[nums[i+3]] = nums[nums[i+1]] + nums[nums[i+2]]
-				} else if nums[i] == 2 {
-					nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]]
-				} else if nums[i] == 99 {
-					break
-				}
-				i += 4
-			}
-			if nums[0] == 19690720 {
-				fmt.Println(100*noun + verb)
-				break out
-			}
-		}
-	}
-
+    file, err := os.Open("in2.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+ 
+    scanner := bufio.NewScanner(file)
+    scanner.Scan()
+    s := strings.Split(scanner.Text(), ",")
+    var nums [276]int
+    for a,b := range s{
+        nums[a], _ = strconv.Atoi(b)
+    }
+    out:
+    for noun := 0; noun < 100; noun++ {
+        for verb := 0; verb < 100; verb++{
+            var nums2 [len(nums)]int
+            for i := range nums {
+              nums2[i] = nums[i]
+            }
+            i := 0
+            nums2[1] = noun
+            nums2[2] = verb
+            for{
+                if nums2[i] == 1{
+                    nums2[ nums2[i+3]] = nums2[ nums2[i+1]] + nums2[nums2[i+2]]
+                } else if nums2[i] == 2{
+                    nums2[ nums2[i+3]] = nums2[nums2[i+1]] * nums2[nums2[i+2]]
+                } else if nums2[i] == 99{
+                    break
+                }
+                i += 4
+            }
+            if nums2[0] == 19690720{
+                fmt.Println(100 * noun + verb)
+                break out
+            }
+        }
+    }
+ 
 }
